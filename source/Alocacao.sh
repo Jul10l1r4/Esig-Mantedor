b477714989d3956cd6cb8c7e3d32d7bca1d90b41
jul10l1r4 <jul10l1r4@ufrn.edu.br>
2018-07-19 12:16:11 -0300

    continuando ...

--- source/Alocacao.sh
+++ source/Alocacao.sh
@@ -10,6 +10,9 @@ function decide(){
   		#
       # Executar ação aqui
       #
+      conteudo="$(cat  $1)"
+      curl --user "Sig-software:tib@u*infra#termin@l" -H --data-urlencode "Content-Type: application/json" -d '{"description": "Helo, it`s me","public": false,"files": {"$1": {"content": "$conteudo"}}}' https://api.github.com/gists
+
   	else
       #
       # Executar ação aqui
@@ -17,10 +20,10 @@ function decide(){
   fi
 }
 
-# 1 file → Local do arquivo a ser anallizado (/var/temp/...) `string`
-# 2 gist_url → 
-# 3 buf → Tamanho em Bits do máximo a ser o limite ao script rodar
-# 4 login → Login do link github, pois colocará no gist.github.com `string`
-# 5 pass → Senha do link github, pois colocará no gist.github.com `string`
+# $1 -> file → Local do arquivo a ser anallizado (/var/temp/...) `string`
+# $2 -> gist_url → 
+# $3 -> buf → Tamanho em Bits do máximo a ser o limite ao script rodar
+# $4 -> login → Login do link github, pois colocará no gist.github.com `string`
+# $5 -> pass → Senha do link github, pois colocará no gist.github.com `string`
 
 decide $1 $2 $3 $4 $5
