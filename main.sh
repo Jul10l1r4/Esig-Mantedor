06322e6355f058466fed25e8ae740b121ec26766
Julio Lira <jul10l1r4@disroot.org>
2020-04-08 14:27:22 -0300

    :tada: Remove unless file

--- main.sh
+++ main.sh
@@ -10,30 +10,12 @@ echo "Ex.: /etc/log/console.log"
 #	pass → Senha do link github, pois colocará no gist.github.com `string`
 
 read -p 'Digite o nome do arquivo (completo):' file
-read -p 'Até quantos bits deseja para o limite (1024B é 1Kb)?: ' buf
 read -p 'Login (do github): ' login
-# Essa porra nao quer pegar
 read -s -p 'Password (do github): ' pass
-
-# Verifica tamanho do arquivo em bits
-arq_bruto=$(stat --printf "%s\n" $file)
-
-# Verifica tamanho do de forma abreviada
-arq_human=$(numfmt --to=iec-i --suffix=b $arq_bruto)
 
 # Começa o arquivo com o padrao de cor branco
 echo "\033[1;37m"
 
-
-# Verifica quantidade e imprime
-if [ $arq_bruto -lt $buf ]
-then
-		echo "\033[1;42;97m É alocavel $arq_human \033[0m"
-		sleep 2
-	else
-		echo "\033[1;41;97m Não tem condições de ser alocado $arq_human \033[0m"
-		sleep 2
-fi
 # Mantendo notificado o da infra
 if [ $USER = "root" ]
 then
@@ -47,7 +29,7 @@ then
 		echo "[ \033[1;32m*\033[0m ] Copiando arquivo para /opt/log-up/"
 		chmod +x /opt/log-up/
 		echo "[ \033[1;32m*\033[0m ] Dando permissoes..."
-		echo "0 *	* * *	root	/opt/log-up/sync_up.sh $file $buf $login $pass" >> /etc/crontab
+		echo "0 *	* * *	root	/opt/log-up/sync_up.sh $file $login \"$pass\"" >> /etc/crontab
 		echo "Agendando crontab, veja em /etc/crontab/:"
 		sleep 1
 		cat /etc/crontab
