8c5c80a313c32faf908a38002731e8f89ddb09de
Julio Lira <jul10l1r4@disroot.org>
2020-04-08 14:28:21 -0300

    :unicorn: Removing unless files

--- source/sync_up.sh
+++ source/sync_up.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
-function decide(){
-  # Passamentos de variáveis
-  arq_bruto=$(stat --printf "%s\n" $1)
-
+function _main(){
   # Verifica quantidade e imprime
-  if [ $arq_bruto -lt $2 ]
-  then
-  		#
-      # Executar ação aqui
-      #
-      conteudo="$(cat  $1)"
-      curl --user "$3:$4" -H --data-urlencode "Content-Type: application/json" -d '{"description": "Arquivo de log $(date)","public": false,"files": {"Log $(date)": {"content": "$conteudo"}}}' https://api.github.com/gists
-      	# Procedurando as coisas, low level is life B-). 
-	# echo -n igual a incrmentar "0 content in file"
-	echo -n > $1
-  	else
-      #
-      # Executar ação aqui
-      #
-  fi
+  # Executar ação aqui
+  #
+  conteudo="$(cat  $1)"
+  curl --user "$2:$3" -H --data-urlencode "Content-Type: application/json" -d "{\"description\": \"Arquivo de log $(hostname) em $1\",\"public\": false,\"files\": {\"Log $(hostname)\": {\"content\": \"$conteudo\"}}}" https://api.github.com/gists
+  # Procedurando as coisas, low level is life B-). 
 }
 
 # $1 -> file → Local do arquivo a ser anallizado (/var/temp/...) `string`
-# $2 -> buf → Tamanho em Bits do máximo a ser o limite ao script rodar
 # $3 -> login → Login do link github, pois colocará no gist.github.com `string`
 # $4 -> pass → Senha do link github, pois colocará no gist.github.com `string`
 
-decide $1 $2 $3 $4
+_main $1 $2 "$3" 
